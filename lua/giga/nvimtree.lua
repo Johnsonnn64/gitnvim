bd3b4f214262cbcc9db7ac71a538b2312f7179fe
Johnsonnn64 <naktacamel2@gmail.com>
2022-05-30 12:45:28 +0800

    migration of nvimtree config because deprecated

--- lua/giga/nvimtree.lua
+++ lua/giga/nvimtree.lua
@@ -1,51 +1,11 @@
-vim.cmd [[
-  let g:nvim_tree_git_hl = 1
-  let g:nvim_tree_highlight_opened_files = 3 "0 by default, will enable folder and file icon highlight for opened files/directories.
-  let g:nvim_tree_add_trailing = 1 "0 by default, append a trailing slash to folder names
-  let g:nvim_tree_group_empty = 1 " 0 by default, compact folders that only contain a single folder into one node in the file tree
-  let g:nvim_tree_icon_padding = ' ' "one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
-  let g:nvim_tree_symlink_arrow = ' >> ' " defaults to ' ➛ '. used as a separator between symlinks' source and target.
-  let g:nvim_tree_respect_buf_cwd = 1 "0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
-  let g:nvim_tree_create_in_closed_folder = 0 "0 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
-  let g:nvim_tree_special_files = { 'README.md': 1, 'Makefile': 1, 'MAKEFILE': 1 } " List of filenames that gets highlighted with NvimTreeSpecialFile
-  let g:nvim_tree_show_icons = {
-    \ 'git': 1,
-    \ 'folders': 1,
-    \ 'files': 1,
-    \ 'folder_arrows': 1,
-    \ }
-  let g:nvim_tree_icons = {
-    \ 'default': "",
-    \ 'symlink': "",
-    \ 'git': {
-    \   'unstaged': "✗",
-    \   'staged': "✓",
-    \   'unmerged': "",
-    \   'renamed': "➜",
-    \   'untracked': "★",
-    \   'deleted': "",
-    \   'ignored': "◌"
-    \   },
-    \ 'folder': {
-    \   'arrow_open': "",
-    \   'arrow_closed': "",
-    \   'default': "",
-    \   'open': "",
-    \   'empty': "",
-    \   'empty_open': "",
-    \   'symlink': "",
-    \   'symlink_open': "",
-    \ }
-  \ }
-]]
-
-
 local status_ok, nvimtree = pcall(require, 'nvim-tree')
 if not status_ok then
   return
 end
 
 nvimtree.setup { -- BEGIN_DEFAULT_OPTS
+  respect_buf_cwd = true,
+  create_in_closed_folder = true,
   auto_reload_on_write = true,
   disable_netrw = false,
   hijack_cursor = true,
@@ -58,7 +18,7 @@ nvimtree.setup { -- BEGIN_DEFAULT_OPTS
   sort_by = "name",
   update_cwd = true,
   view = {
-    width = 28,
+    width = 32,
     height = 30,
     side = "left",
     preserve_window_proportions = false,
@@ -97,6 +57,45 @@ nvimtree.setup { -- BEGIN_DEFAULT_OPTS
     },
   },
   renderer = {
+    group_empty = true,
+    add_trailing = true,
+    highlight_git = true,
+    highlight_opened_files = "all",
+    special_files = {
+
+    },
+    icons = {
+      padding = " ",
+      show = {
+      git = true,
+      file = true,
+      folder = true,
+      folder_arrow = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌"
+        },
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+      },
+    },
     indent_markers = {
       enable = true,
       icons = {
@@ -150,3 +149,12 @@ nvimtree.setup { -- BEGIN_DEFAULT_OPTS
 vim.cmd [[
   autocmd BufEnter * silent if (winnr("$") == 1 && &filetype == "nvimtree") | q | endif
 ]]
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	pattern = { "*" },
+	nested = true,
+	callback = function()
+		if vim.fn.winnr "$" == 1 and vim.fn.bufname() == "NvimTree_" .. vim.fn.tabpagenr() then
+			vim.api.nvim_command ":silent qa!"
+		end
+	end,
+})
