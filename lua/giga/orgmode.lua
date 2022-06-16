d2ef979f47f4a754157dabc3dfa66dd77f9ee07b
Johnsonnn64 <naktacamel2@gmail.com>
2022-06-16 15:31:31 +0800

    org agenda start date monday->sunday

--- lua/giga/orgmode.lua
+++ lua/giga/orgmode.lua
@@ -4,6 +4,7 @@ require('orgmode').setup_ts_grammar()
 require('orgmode').setup({
   org_agenda_files = {'~/personal/org/*'},
   org_default_notes_file = '~/personal/org/index.org',
+  org_agenda_start_on_weekday = 7,
 })
 
 -- bullet symbols
