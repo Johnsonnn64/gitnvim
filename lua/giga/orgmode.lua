a7ff1c9304cb6c73f80e8d24d1a73f9b8c7ffb28
Johnsonnn64 <naktacamel2@gmail.com>
2022-08-12 15:27:13 +0800

    calendar week start day sunday

--- lua/giga/orgmode.lua
+++ lua/giga/orgmode.lua
@@ -5,6 +5,8 @@ require('orgmode').setup({
   org_agenda_files = {'~/personal/org/*'},
   org_default_notes_file = '~/personal/org/index.org',
   org_agenda_start_on_weekday = 7,
+  -- org_todo_keywords = {'TODO', 'WAITING', '|', 'DONE'},
+  calendar_week_start_day = 0,
 })
 
 -- bullet symbols
