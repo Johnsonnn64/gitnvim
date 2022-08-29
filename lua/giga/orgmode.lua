274dec82c4f8e9fbc3844cf0d3c1305ec8fd61b8
Johnsonnn64 <naktacamel2@gmail.com>
2022-08-29 09:45:56 +0800

    org config change

--- lua/giga/orgmode.lua
+++ lua/giga/orgmode.lua
@@ -5,8 +5,10 @@ require('orgmode').setup({
   org_agenda_files = {'~/personal/org/*'},
   org_default_notes_file = '~/personal/org/index.org',
   org_agenda_start_on_weekday = 7,
+  org_agenda_skip_deadline_if_done = true,
   -- org_todo_keywords = {'TODO', 'WAITING', '|', 'DONE'},
   calendar_week_start_day = 0,
+  win_split_mode = "edit",
 })
 
 -- bullet symbols
