1f5a4f00b77c7a7f233780b3c461512d5a36fb62
Johnsonnn64 <naktacamel2@gmail.com>
2022-08-29 09:00:09 +0800

    comment packer autoinstall (moved to Makefile in config auto install)

--- lua/giga/plugins.lua
+++ lua/giga/plugins.lua
@@ -1,14 +1,13 @@
-local fn = vim.fn
+-- local fn = vim.fn
 
 -- auto install packer.nvim
-local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path,
-  }
-  print 'Installing packer... reopen Nvim'
-  vim.cmd [[packadd packer.nvim]]
-end
+-- local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+-- if fn.empty(fn.glob(install_path)) > 0 then
+--   PACKER_BOOTSTRAP = fn.system {
+--     'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path,
+--   }
+--   print 'Installing packer... reopen Nvim'
+-- end
 
 -- auto sync packer
 vim.cmd [[
