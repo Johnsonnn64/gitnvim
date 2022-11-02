c8ad58db64ab516f30a9fe1df6e92b0c575c72fe
Johnsonnn64 <naktacamel2@gmail.com>
2022-11-02 12:20:05 +0800

    format cleanup

--- lua/giga/telescope.lua
+++ lua/giga/telescope.lua
@@ -1,4 +1,4 @@
-local status_ok, telescope = pcall(require,'telescope')
+local status_ok, telescope = pcall(require, 'telescope')
 if not status_ok then
   return
 end
@@ -16,7 +16,7 @@ telescope.setup {
       "--hidden"
     },
     path_display = { truncate = 2 },
-    file_ignore_patterns = {"%.git", "%.pki", "%.jpg", "%.jpeg", "%.png", "%.gif", "%.otf", "%.ttf"},
+    file_ignore_patterns = { "%.git", "%.pki", "%.jpg", "%.jpeg", "%.png", "%.gif", "%.otf", "%.ttf" },
     preview = {
       timeout = 200,
       filesize_limit = 3
@@ -40,5 +40,5 @@ telescope.setup {
   },
 }
 -- require"telescope".load_extension("frecency")
-require"telescope".load_extension("ui-select")
+require("telescope").load_extension("ui-select")
 require("telescope").load_extension("notify")
