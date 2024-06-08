0bae0aa25ed60f5fd4a74e9fe413162d11ded582
JohnChad <naktacamel2@gmail.com>
2024-06-08 15:28:41 -0700

    load priority of autocmd changed

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
+vim.loader.enable()
 require 'giga.plugins'
+require 'giga.autocmd'
 require 'giga.options'
 require 'giga.keymaps'
 require 'giga.colorscheme'
 require 'giga.lualine'
 require 'giga.treesitter'
 require 'giga.indentline'
-require 'giga.autocmd'
