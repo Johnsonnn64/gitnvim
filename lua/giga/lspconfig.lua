0cee2b431d80a44ae401eec40f26a6bb35955b8a
John Cha <naktacamel2@gmail.com>
2025-08-25 23:16:23 -0700

    change location of lspconfig

---
+++ lua/giga/lspconfig.lua
@@ -0,0 +1,12 @@
+require ('mason').setup()
+-- require("mason-lspconfig").setup({
+--   ensure_installed = lsp_list.lsp_list
+-- })
+
+-- require('lspconfig')
+-- for i=1, #lsp_list.lsp_list do
+--   require("lspconfig")[lsp_list.lsp_list[i]].setup({
+--     on_attach = require('giga.lsp.handlers').on_attach,
+--     capabilities = require('giga.lsp.handlers').capabilities,
+--   })
+  -- end
