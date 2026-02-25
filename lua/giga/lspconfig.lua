216540c416ac6c47be08855107648049a54a534e
John Cha <naktacamel2@gmail.com>
2026-02-24 21:19:18 -0800

    shit isnt needed
    a

--- lua/giga/lspconfig.lua
+++ lua/giga/lspconfig.lua
@@ -1,14 +1,5 @@
 require ('mason').setup()
 
-vim.lsp.enable({
-  "lua_ls",
-  "bashls",
-  "clangd",
-  "marksman",
-  "pyright",
-  -- "jdtls",
-})
-
 require ('giga.lsp.handlers').setup()
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup('lsp-attach', {clear = true}),
